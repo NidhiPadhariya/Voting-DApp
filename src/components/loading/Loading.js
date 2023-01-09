47aad6923d9d9635fb68593a3c4d248e34105153
Nidhi_Padhariya <nidhi.padhariya@neosoftmail.com>
2023-01-09 13:08:42 +0530

    Changed Loading text

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -8,9 +8,9 @@ function Loading({ text }) {
         <>
             <div className="loading-container">
                 <div className="load load1"></div>
-                <Box width="20" />
+                <Box width="10" />
                 <div className="load load2"></div>
-                <Box width="20" />
+                <Box width="10" />
                 <div className="load load3"></div>
             </div>
             <div className="loading-container loading-text">{text}</div>
